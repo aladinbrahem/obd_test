a07bff35704a18453d6c52e2ac24bfb5d20798ae
Alaeddine Brahem <41130770+aladinbrahem@users.noreply.github.com>
2020-02-24 11:43:24 +0100

    Update Select.py

--- Select.py
+++ Select.py
@@ -1,28 +1,3 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#/***************************************************************************/
-#/* Raspberry Pi ELM327 OBBII CAN BUS Diagnostic Software.                  */
-#/*                                                                         */
-#/* (C) Jason Birch 2018-05-15 V1.04                                        */
-#/*                                                                         */
-#/* Class: Select                                                           */
-#/* Display a dialog showing a list of text and get the line number of the  */
-#/* text which the user click on.                                           */
-#/***************************************************************************/
-
-
-
 import pygame
 import Visual
 import Button
