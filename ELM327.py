29ac5938e6883d3fcb5aa9236c1c6f1cd59e2110
Alaeddine Brahem <41130770+aladinbrahem@users.noreply.github.com>
2020-02-24 11:41:25 +0100

    Update ELM327.py

--- ELM327.py
+++ ELM327.py
@@ -1,28 +1,3 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-#/***************************************************************************/
-#/* Raspberry Pi ELM327 OBBII CAN BUS Diagnostic Software.                  */
-#/*                                                                         */
-#/* (C) Jason Birch 2018-05-29 V1.09                                        */
-#/*                                                                         */
-#/* Class: ELM327                                                           */
-#/* Handle communications with an ELM327 device, communicating with the     */
-#/* CAN BUS on a car ECU.                                                   */
-#/***************************************************************************/
-
-
-
 import time
 import serial
 
